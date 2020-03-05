967df143840b1b8563e5db7f8df6e9ac0c1519c7
Saref111 <saref012@gmail.com>
2020-03-05 12:58:16 +0200

    Added listener for error message button

--- js/message.js
+++ js/message.js
@@ -3,11 +3,13 @@
 (function () {
   var errorModal = document.querySelector('#error').content.querySelector('.error');
   var message = errorModal.querySelector('.error__message');
+  var errorButton = errorModal.querySelector('.error__button');
   var successModal = document.querySelector('#success').content.querySelector('.success');
 
   var hideErrorModalHandler = function () {
     errorModal.remove();
     document.removeEventListener('mousedown', hideErrorModalHandler);
+    errorButton.removeEventListener('click', hideErrorModalHandler);
   };
 
   var hideSuccessModalHandler = function () {
@@ -19,6 +21,7 @@
     message.textContent = errorMassage;
     document.querySelector('main').insertAdjacentElement('afterbegin', errorModal);
     document.addEventListener('mousedown', hideErrorModalHandler);
+    errorButton.addEventListener('click', hideErrorModalHandler);
     document.addEventListener('keydown', function (evt) {
       if (evt.key === 'Escape') {
         hideErrorModalHandler();
